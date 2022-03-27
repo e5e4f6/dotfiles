f8f8f4aec254e22e389d8d4ff856bbcb1facb770
Overrings & Umlauts <e5e4f6@nascentmaker.com>
2022-03-27 00:05:59 -0400

    initial commit

---
+++ dot_config/powershell/profile.ps1
@@ -0,0 +1,48 @@
+# -*-mode:powershell-*- vim:ft=powershell
+
+# ~/.config/powershell/profile.ps1
+# =============================================================================
+# Executed when PowerShell starts.
+#
+# On Windows, this file will be copied over to these locations after
+# running `chezmoi apply` by the script `../../run_powershell.bat.tmpl`:
+#     - %USERPROFILE%\Documents\PowerShell
+#     - %USERPROFILE%\Documents\WindowsPowerShell
+#
+# See https://docs.microsoft.com/en/powershell/module/microsoft.powershell.core/about/about_profiles
+
+# Determine user profile parent directory.
+$ProfilePath=Split-Path -parent $profile
+
+# Load functions declarations from separate configuration file.
+if (Test-Path $ProfilePath/functions.ps1) {
+    . $ProfilePath/functions.ps1
+}
+
+if (Test-Path "$env:AppData\gnupg") {
+    $env:GNUPGHOME = "$env:AppData\gnupg"
+    [System.Environment]::SetEnvironmentVariable('GNUPGHOME', $env:GNUPGHOME)
+}
+
+$gpgcommand = Invoke-Command -ScriptBlock {
+    scoop which gpg
+}
+git config --global gpg.program "$gpgcommand"
+
+# Add missing user paths.
+# if (Get-Command Add-EnvPath -errorAction Ignore) {
+#     if ($IsWindows) {
+#         Add-EnvPath -Path "${Env:Programfiles}\Git\cmd\" -Position "Append"
+#     }
+#     else {
+#         Add-EnvPath -Path "/usr/local/sbin" -Position "Prepend"
+#         Add-EnvPath -Path "/usr/local/bin" -Position "Prepend"
+#     }
+# }
+
+# Load alias definitions from separate configuration file.
+if (Test-Path $ProfilePath/aliases.ps1) {
+    . $ProfilePath/aliases.ps1
+}
+
+Invoke-Expression (&starship init powershell)
---
+++ dot_config/powershell/setup.ps1
@@ -0,0 +1,158 @@
+# -*-mode:powershell-*- vim:ft=powershell
+
+# ~/.config/powershell/setup.ps1
+# =============================================================================
+# Idempotent manual setup script to install or update Powershell dependencies.
+#
+# On Windows, this file will be copied over to these locations after
+# running `chezmoi apply` by the script `../../run_powershell.bat.tmpl`:
+#     - %USERPROFILE%\Documents\PowerShell
+#     - %USERPROFILE%\Documents\WindowsPowerShell
+#
+# TODO: Convert this to `~/dotfiles.ps1`
+
+# Requires that PowerShell be running with elevated privileges to be able to
+# change system properties.
+#Disabled Requires -RunAsAdministrator
+
+# Create missing $IsWindows if running Powershell 5 or below.
+if (!(Test-Path variable:global:IsWindows)) {
+    Set-Variable "IsWindows" -Scope "Global" -Value ([System.Environment]::OSVersion.Platform -eq "Win32NT")
+}
+
+if ($null -eq (Get-Variable "ColorInfo" -ErrorAction "Ignore")) {
+    Set-Variable -Name ColorInfo -Value "DarkYellow"
+}
+Set-Variable -Name count -Value 0 -Scope Script
+
+function eos {
+    <#
+    .SYNOPSIS
+        Terminates the script and counts the actions taken.
+    .INPUTS
+        None
+    .OUTPUTS
+        None
+    #>
+    if ($count) {
+        Write-Host "Done! $count modification(s) applied." -ForegroundColor $ColorInfo
+    }
+    else {
+        Write-Host "No modifications performed." -ForegroundColor $ColorInfo
+    }
+    Remove-Variable -Name count -Scope Script
+}
+
+# Ask for confirmation.
+$hereString = "
+    This script will perform the following non-destructive adjustements to the system (if required):
+        - Install package provider NuGet
+        - Install/update Powershell modules"
+if ($IsWindows) {
+    $hereString += "
+        - Enable LongPaths support for file paths above 260 characters"
+}
+$hereString += [Environment]::NewLine
+Write-Host $hereString -ForegroundColor $ColorInfo
+$confirmation = Read-Host -Prompt "Do you want to proceed? [y/N]"
+if ($confirmation -notMatch '^y(es)?$') {
+    eos
+    exit
+}
+Remove-Variable -Name ("confirmation", "hereString")
+
+#
+# Dependencies
+#
+
+# Install NuGet package manager required to install WSL Interop and others.
+# See https://www.nuget.org/
+if (Get-PackageProvider -ListAvailable -Name NuGet -ErrorAction "Ignore") {
+    Write-Host "NuGet already installed, skipping." -ForegroundColor $ColorInfo
+}
+else {
+    Write-Host "Installing NuGet..." -ForegroundColor $ColorInfo
+    Install-PackageProvider -Name NuGet -Force
+    $count++
+}
+
+# Setup Scoop.
+# See https://github.com/lukesampson/scoop
+if ($IsWindows) {
+    if (!(Get-Command "scoop" -ErrorAction "Ignore")) {
+        Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+    }
+    if (Get-Command "scoop" -ErrorAction "Ignore") {
+        Write-Host "Verifying the state of Scoop..." -ForegroundColor $ColorInfo
+        Get-Command -Name scoop -ErrorAction Stop
+        Invoke-Command -ScriptBlock { scoop checkup }
+
+        # Install any missing bucket.
+        $bucketList = Invoke-Command -ScriptBlock { scoop bucket list }
+        $buckets = (
+            "extras",
+            "nerd-fonts"
+        )
+        $buckets | ForEach-Object {
+            if (!$bucketList.Contains($_)) {
+                Invoke-Command -ScriptBlock { scoop bucket add $_ }
+            }
+        }
+
+        # Install any missing app.
+        $appList = Invoke-Command -ScriptBlock { scoop export }
+        $appList = $appList -replace "[\s].+",""
+        $apps = (
+            "1password-cli",
+            "7zip",
+            "chezmoi",
+            "dark",
+            "extras/gpg4win",
+            "extras/vcredist2022",
+            "gh",
+            "git",
+            "innounp",
+            "less",
+            "lessmsi",
+            "neofetch",
+            "neovim",
+            "nerd-fonts/CascadiaCode-NF",
+            "nodejs-lts",
+            "putty",
+            "python",
+            "ripgrep",
+            "starship",
+            "wget"
+        )
+        $apps | ForEach-Object {
+            if (!$appList.Contains($_)) {
+                Invoke-Command -ScriptBlock { scoop install $_ }
+                $count++
+            }
+        }
+        Invoke-Command -ScriptBlock { scoop uninstall extras/vcredist2022 }
+    }
+}
+
+
+#
+# System tweaks
+#
+
+# Enable LongPaths support for file paths above 260 characters.
+# See https://social.msdn.microsoft.com/Forums/en-US/fc85630e-5684-4df6-ad2f-5a128de3deef
+if ($IsWindows) {
+    $property = 'HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem'
+    $name = 'LongPathsEnabled'
+    if ((Get-ItemPropertyValue $property -Name $name) -ne 0) {
+        Write-Host "LongPaths support already enabled, skipping." -ForegroundColor $ColorInfo
+    }
+    else {
+        Write-Host "Enabling LongPaths support for file paths above 260 characters." -ForegroundColor $ColorInfo
+        Set-ItemProperty $property -Name $name -Value 1
+        $count++
+    }
+}
+
+# Display termination message.
+eos
